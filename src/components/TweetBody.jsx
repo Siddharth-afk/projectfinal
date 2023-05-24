d00cee9471e9b28dcb4cae02fcba44bfb4644a35
h <h@gmail.com>
2023-05-24 19:06:55 +0530

    worked on the frontend

--- src/components/TweetBody.jsx
+++ src/components/TweetBody.jsx
@@ -15,7 +15,7 @@ function TweetBody({ data }) {
     const subjectivitypArray = Object.entries(data.Subjectivity).map(([key, value]) => ({ id: key, subjectivity: value }));
     const subjectivitynArray = Object.entries(data.Subjectivity).map(([key, value]) => ({ id: key, subjectivity: value }));
     subjectivitypArray.sort((a, b) => b.subjectivity - a.subjectivity);
-    subjectivitynArray.sort((a, b) => a.subjectivity - b.subjectivity);
+    subjectivitynArray.sort((a, b) => a.subjectivity - b.subjectivity);   
 
     const top5pTweets = subjectivitypArray.slice(0, 5).map(item => {
         const tweetId = item.id;
@@ -35,6 +35,10 @@ function TweetBody({ data }) {
     const neg = data['perneg'][0]
     const net = data['pernet'][0]
 
+    const net_para = "The prevalence of neutral sentiment outweighs both positive and negative sentiment, suggesting that the stock may remain unaffected by the current tweets. The abundance of neutral tweets implies a lack of strong sentiment, potentially indicating limited impact on the stock's performance."
+    const pos_para = "With positive sentiment surpassing both negative and neutral sentiment, the stock could potentially experience a positive response to the ongoing tweets. The prevalence of positive tweets indicates a favorable outlook, suggesting that the stock may be influenced by the overall optimistic sentiment expressed on social media."
+    const neg_para = "Negative sentiment outweighs both positive and neutral sentiment, the stock may be susceptible to negative implications resulting from the ongoing tweets. The predominance of negative tweets suggests a pessimistic sentiment, which could potentially impact the stock's performance adversely. Investors should be cautious in such scenarios."
+    const inconclusive = "Tweet data is inconclusive, Meaning it becomes difficult to predict the potential impact of current tweets on the stock. The absence of a clear majority in positive, negative, or neutral sentiment implies uncertainty in assessing the sentiment's influence. Investors may need to rely on additional factors or analysis to make informed decisions regarding the stock's response to the tweets."
 
     return (
         <div>
@@ -87,8 +91,12 @@ function TweetBody({ data }) {
                     <p className='font-bold text-xl mt-2'>{neg}</p>
                 </div>          
             </div>
-        </div>
-        
+
+            <div className='text-white mx-auto my-auto px-5 mt-10'>
+                <h2 className='font-bold mb-3 text-xl'>Conclusion</h2>
+                {net > pos && neg ? <p>{net_para}</p> : pos > net & neg ? <p>{pos_para}</p> : neg > pos && net ? <p>{neg_para}</p> : <p>{inconclusive}</p>}
+            </div>
+        </div>      
     )
 }
 
